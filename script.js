7f17b6ae61c023b311789f014ab57936aae2366f
SaiRuthwik22 <160124679+SaiRuthwik22@users.noreply.github.com>
2024-06-08 12:52:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+let element = document.getElementById("level")
+let count = 0;
+while (element.parentNode != null) {
+    if (element.parentNode.nodeName == "HTML") {
+        count++;
+        alert("The level of the element is: " + count);
+        break;
+    }
+    else {
+        count++;
+        element = element.parentNode;
+    }
